c15a81cc464f8d3a0c089d09f9c6f645b1d6dc32
Cooper-GaminG <70198785+Cooper-GaminG@users.noreply.github.com>
2021-01-18 10:21:38 +0100

    CRUD actions are in

--- main.php
+++ main.php
@@ -30,28 +30,66 @@ try {
   $pages = $stmt->fetchAll();
 
   //CREATES A NEW USER INTO THE 'users' DATABASE
-  //PASSWORD IS NOG NIET BEVEILIGD!!!
-  $username = 'Kevin';
-  $password = 'Kevin07!';
-  $email = 'kevin07@gmail.com';
-  $role = 'user';
+  //PASSWORD IS NOG NIET BEVEILIGD
+  // $username = '';
+  // $password = '';
+  // $email = '';
+  // $role = '';
 
-  $sql = 'INSERT INTO users(username, password, email, role) VALUES(:username, :password, :email, :role)';
-  $stmt = $conn->prepare($sql);
-  $stmt->execute(['username' => $username, 'password' => $password, 'email' => $email, 'role' => $role]);
-  echo 'User Created';
+  // $sql = 'INSERT INTO users(username, password, email, role) VALUES(:username, :password, :email, :role)';
+  // $stmt = $conn->prepare($sql);
+  // $stmt->execute(['username' => $username, 'password' => $password, 'email' => $email, 'role' => $role]);
+  // echo 'User Created';
 
-  //$sql is bij mij $conn
+  //DELETES USER
+  // $username = 'Kevin';
+
+  // $sql = 'DELETE FROM users WHERE username = :username';
+  // $stmt = $conn->prepare($sql);
+  // $stmt->execute(['username' => $username]);
+  // echo 'User Deleted';
+
+  //$sql is hier $conn
+  //CREATE
   //INSERT POST THROUGH CODE, THIS SHOULD HAPPEN; ECHO->POST ADDED AND IT SHOULD CREATE A NEW PAGE INSIDE THE DATABASE
-  $title = 'Post 3';
-  $content = 'This is post 3';
-  //maybe, every user gets his own author number, they fill that in and it'll show the same way as it shows the users username
-  $author = '';
 
-  $sql = 'INSERT INTO pages(title, content, author) VALUES(:title, :content, :author)';
+  // $title = '';
+  // $content = '';
+  // //maybe, every user gets his own author number, they fill that in and it'll show the same way as it shows the users username
+  // $author = '';
+
+  // $sql = 'INSERT INTO pages(title, content, author) VALUES(:title, :content, :author)';
+  // $stmt = $conn->prepare($sql);
+  // $stmt->execute(['title' => $title, 'content' => $content, 'author' => $author]);
+  // echo 'Post Added';
+
+  //UPDATE
+  // $id = 32;
+  // $content = 'This is the updated post';
+
+  // $sql = 'UPDATE pages SET content = :content WHERE id = :id';
+  // $stmt = $conn->prepare($sql);
+  // $stmt->execute(['content' => $content, 'id' => $id]);
+  // echo 'Post Updated';
+
+  //DELETE
+  // $id = 32;
+
+  // $sql = 'DELETE FROM pages WHERE id = :id';
+  // $stmt = $conn->prepare($sql);
+  // $stmt->execute(['id' => $id]);
+  // echo 'Post Deleted';
+
+  //READ, search for posts through database
+  $search = '%t%';
+  $sql = 'SELECT * FROM pages WHERE title LIKE ?';
   $stmt = $conn->prepare($sql);
-  $stmt->execute(['title' => $title, 'content' => $content, 'author' => $author]);
-  echo 'Post Added';
+  $stmt->execute([$search]);
+  $pages = $stmt->fetchAll();
+
+  foreach($pages as $page){
+    echo $page['title'] . '<br>' ;
+  }
 
   //var_dump($users);
 
@@ -79,14 +117,14 @@ ARRAY = in principe gewoon een LIJST
 -->
 
 <?php
-if(is_array($pages))
-  foreach( $pages as $page ) {
-    //var_dump($user);
-    echo "<p>" . $page->id . "</p>";
-    echo "<p>" . $page->username . "</p>";
-    echo "<h2>" . $page->title . "</h2>";
-    echo "<p>" . $page->content . "</p>";
-  }
+// if(is_array($pages))
+//   foreach( $pages as $page ) {
+//     //var_dump($user);
+//     echo "<p>" . $page->id . "</p>";
+//     echo "<p>" . $page->username . "</p>";
+//     echo "<h2>" . $page->title . "</h2>";
+//     echo "<p>" . $page->content . "</p>";
+//   }
 
 ?>
 
